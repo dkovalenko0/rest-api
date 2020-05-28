157b839ce10b1e0883322d89b7fdb3853eda1918
dkovalenko0 <kovalenkodima715@gmail.com>
2020-05-28 11:22:03 +0300

    first commit

---
+++ client/frontend.js
@@ -0,0 +1,42 @@
+import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js'
+
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            form: {
+                name: '',
+                value: ''
+            },
+            contacts: [
+                {
+                    id: 1,
+                    name: 'Dima',
+                    value: '+380730480599',
+                    marked: false
+                }
+            ]
+        }
+    },
+    computed: {
+      canCreate() {
+          return this.form.value.trim() && this.form.name.trim()
+      }
+    },
+    methods: {
+        createContact() {
+            const {...contact} = this.form
+            this.contacts.push({...contact, id: Date.now(), marked: false})
+
+            this.form.name = ''
+            this.form.value = ''
+        },
+        markContact(id) {
+            const contact = this.contacts.find(c => c.id === id)
+            contact.marked = true
+        },
+        removeContact(id) {
+            this.contacts = this.contacts.filter(c => c.id !== id)
+        }
+    }
+})
